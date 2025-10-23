bfa0a646013c43cde7eee746875a330af2288cd9
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-10-23 16:25:57 +0700

    Tugas4

--- 20230140187-server/server.js
+++ 20230140187-server/server.js
@@ -1,37 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const presensiRoutes = require('./routes/presensi');
-const reportRoutes = require('./routes/reports');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const PORT = 3001;
+const morgan = require("morgan");
+
+// Impor router
+const presensiRoutes = require("./routes/presensi");
+const reportRoutes = require("./routes/reports");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use('/api/presensi', presensiRoutes);
-app.use('/api/reports', reportRoutes);
-
-// Logging middleware
+app.use(morgan("dev"));
 app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
     next();
 });
-
-// Routes
-const bookRoutes = require('./routes/books');
-app.use('/api/books', bookRoutes);
-
-// 404 Middleware
-app.use((req, res, next) => {
-    res.status(404).json({ message: 'Route not found' });
+app.get("/", (req, res) => {
+    res.send("Home Page for API");
 });
-
-// Global Error Handler
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Internal Server Error' });
-});
-
+const ruteBuku = require("./routes/books");
+app.use("/api/books", ruteBuku);
+app.use("/api/presensi", presensiRoutes);
+app.use("/api/reports", reportRoutes);
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+    console.log(`Express server running at http://localhost:${PORT}/`);
 });
---
+++ tugas/Tugas4.md
@@ -0,0 +1,19 @@
+# Tugas 4 - Express.js API Presensi
+
+- *Nama:* Irfan Afifuddin  
+- *NIM:* 20230140187  
+- *Kelas:* D
+
+## Endpoint Presensi & Reports
+
+### 1. POST /api/presensi/check-in
+![CHECK-IN](./screenshot/CHECKIN4.png)
+
+### 2. POST /api/presensi/check-out
+![CHECK-OUT](./screenshot/CHECKOUT4.png)
+
+### 3. GET /api/reports/daily
+![REPORTS-DAILY](./screenshot/REPORTDAILY4.png)
+
+### 4. DATABASE
+![REPORTS-DAILY](./screenshot/DATABASE.png)
