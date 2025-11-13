6c245db6c608d145081b51716172888ae41b0add
IrfanAfifuddin <afifuddinirfanafif@gmail.com>
2025-11-13 16:03:17 +0700

    fix: remove sublink

---
+++ 20230140187-react/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import DashboardPage from './components/DashboardPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav className="p-4 bg-gray-100 text-center">
+          <Link to="/login" className="mr-4 text-blue-600 hover:underline">
+            Login
+          </Link>
+          <Link to="/register" className="text-blue-600 hover:underline">
+            Register
+          </Link>
+        </nav>
+
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
